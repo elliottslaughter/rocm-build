d885e3cd4b4c3a6f5c0a629d6fc7ce07bed5e7d5
xuhuisheng <xyz20003@gmail.com>
2021-10-28 14:27:02 +0800

    hip

--- 19.hip.sh
+++ 19.hip.sh
@@ -2,28 +2,28 @@
 
 set -e
 
-sudo apt install -y rpm libfile-which-perl kmod doxygen
-sudo apt install -y libfile-basedir-perl libfile-copy-recursive-perl libfile-listing-perl libhttp-date-perl libipc-system-simple-perl libtimedate-perl liburi-encode-perl
+sudo apt install -y dpkg-dev rpm doxygen libelf-dev rename liburi-encode-perl \
+                    libfile-basedir-perl libfile-copy-recursive-perl libfile-listing-perl
 
 mkdir -p $ROCM_BUILD_DIR/hip
 cd $ROCM_BUILD_DIR/hip
 pushd .
 
+ROCclr_DIR=$ROCM_GIT_DIR/ROCclr
+OPENCL_DIR=$ROCM_GIT_DIR/ROCm-OpenCL-Runtime
+HIP_DIR=$ROCM_GIT_DIR/HIP
+
 START_TIME=`date +%s`
 
 cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DHIP_COMPILER=clang \
-    -DHIP_PLATFORM=amd \
-    -DROCM_PATH=$ROCM_INSTALL_DIR \
-    -DHSA_PATH=$ROCM_INSTALL_DIR/hsa \
-    -DCPACK_INSTALL_PREFIX=$ROCM_INSTALL_DIR/hip \
-    -DCMAKE_PREFIX_PATH="$ROCM_BUILD_DIR/rocclr;$ROCM_INSTALL_DIR" \
-    -DCMAKE_INSTALL_PREFIX=$ROCM_INSTALL_DIR/hip/ \
-    -DCPACK_PACKAGING_INSTALL_PREFIX=$ROCM_INSTALL_DIR/hip/ \
-    -DCPACK_GENERATOR=DEB \
+    -DHIP_COMMON_DIR="$HIP_DIR" \
+    -DAMD_OPENCL_PATH="$OPENCL_DIR" \
+    -DROCCLR_PATH="$ROCCLR_DIR" \
+    -DCMAKE_PREFIX_PATH="$ROCM_INSTALL_DIR" \
+    -DCMAKE_BUILD_TYPE=Release
     -G Ninja \
-    $ROCM_GIT_DIR/HIP
+    $ROCM_GIT_DIR/hipamd
+
 ninja
 sudo ninja install
 ninja package
